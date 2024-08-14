2ecd6ab069ab4d00610e1bdc585ee933bd8af2d2
shubhamkushwah123 <shubhamkushwah123@gmail.com>
2024-08-14 16:34:50 -0500

    headless mode added

--- src/main/java/com/testing/insure_me_test_app/App.java
+++ src/main/java/com/testing/insure_me_test_app/App.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Hello world!
@@ -15,8 +16,10 @@ public class App
     public static void main( String[] args ) throws InterruptedException
     {
 //initialize the chrome driver
-		
-	 	WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+    	options.addArguments("--headless");
+    	
+	 	WebDriver driver = new ChromeDriver(options);
 	 	
 	 	//open the web application
         driver.get("http://18.222.0.17:8081/contact.html");
@@ -28,28 +31,25 @@ public class App
         //1. Locate and Enter Name
         WebElement name = driver.findElement(By.id("inputName"));
         name.sendKeys("Shubham");
-        System.out.println(name);
         
-        Thread.sleep(2000);
+        String printName = driver.findElement(By.id("inputName")).getText();
+        
+        System.out.println();
         
         //2. Locate and Enter Contact
         WebElement number = driver.findElement(By.id("inputNumber"));
         number.sendKeys("999999999");
-        System.out.println(number);
-        
-        Thread.sleep(2000);
+        System.out.println(number.getText().toString());
         
         //3. Locate Email and Enter Email
         WebElement mail = driver.findElement(By.id("inputMail"));
         mail.sendKeys("shubham@xyz.com");
-        System.out.println(mail);
-        
-        Thread.sleep(2000);
+        System.out.println(mail.getText());
         
         //4. Locate Message and Enter Message
         WebElement message = driver.findElement(By.id("inputMessage"));
         message.sendKeys("Hello, How are you ?");
-        System.out.println(message);
+        System.out.println(message.getText());
         
         Thread.sleep(2000);
         
