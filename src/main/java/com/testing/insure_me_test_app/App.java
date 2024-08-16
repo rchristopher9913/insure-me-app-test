dd4557dc2a2b2679a0bf032f66ee0c211ec517c9
rchristopher9913 <christopher.rajakumar@tcs.com>
2024-08-16 14:42:37 -0400

    Updated the app url

--- src/main/java/com/testing/insure_me_test_app/App.java
+++ src/main/java/com/testing/insure_me_test_app/App.java
@@ -15,14 +15,14 @@ public class App
 
     public static void main( String[] args ) throws InterruptedException
     {
-//initialize the chrome driver
-		ChromeOptions options = new ChromeOptions();
+	//initialize the chrome driver
+	ChromeOptions options = new ChromeOptions();
     	options.addArguments("--headless");
     	
-	 	WebDriver driver = new ChromeDriver(options);
+	 WebDriver driver = new ChromeDriver(options);
 	 	
-	 	//open the web application
-        driver.get("http://18.222.0.17:8081/contact.html");
+	 //open the web application
+        driver.get("http://18.226.28.50:8082/contact.html");
         
         
         String title = driver.getTitle();
@@ -30,35 +30,33 @@ public class App
         
         //1. Locate and Enter Name
         WebElement name = driver.findElement(By.id("inputName"));
-        name.sendKeys("Shubham");
-        
-        String printName = driver.findElement(By.id("inputName")).getText();
-        
-        System.out.println();
-        
+        name.sendKeys("Christopher");
+        System.out.println(name.getText().toString());
+
         //2. Locate and Enter Contact
         WebElement number = driver.findElement(By.id("inputNumber"));
-        number.sendKeys("999999999");
+        number.sendKeys("911");
         System.out.println(number.getText().toString());
         
         //3. Locate Email and Enter Email
-        WebElement mail = driver.findElement(By.id("inputMail"));
-        mail.sendKeys("shubham@xyz.com");
+        WebElement mail = driver.findElement(By.id("inputEmail"));
+        mail.sendKeys("christopher@xyz.com");
         System.out.println(mail.getText());
         
         //4. Locate Message and Enter Message
         WebElement message = driver.findElement(By.id("inputMessage"));
         message.sendKeys("Hello, How are you ?");
         System.out.println(message.getText());
-        
+
         Thread.sleep(2000);
         
         //5. Locate Send Button and click to send the message.
-        WebElement sendButton = driver.findElement(By.id("my-button"));
+        WebElement sendButton = driver.findElement(By.id("send-button"));
         sendButton.click();
+
         Thread.sleep(2000);
         
-        String response = driver.findElement(By.id("response")).getText();
+        String response = driver.findElement(By.id("message")).getText();
         System.out.println(response);
         
         
